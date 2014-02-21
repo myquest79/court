b9b5f2fb4af00acd673771a47f9bb7a2878f5a16
miwest <miwest@adobe.com>
2014-02-20 19:46:43 -0700

    Created ReservationService

---
+++ src/main/java/com/apress/springrecipes/court/service/ReservationService.java
@@ -0,0 +1,10 @@
+package com.apress.springrecipes.court.service;
+
+import java.util.List;
+
+import com.apress.springrecipes.court.domain.Reservation;
+
+public interface ReservationService 
+{
+	public List<Reservation> query(String courtName);
+}
